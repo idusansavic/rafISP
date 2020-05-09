42c8812aece5de75318fbb3f1ea9d3944ab936a5
idusansavic <54639922+idusansavic@users.noreply.github.com>
2020-05-09 19:08:04 +0200

    InternetProvajder TarifniPaket return empty string

--- rafISP.php
+++ rafISP.php
@@ -4,15 +4,15 @@
 require_once "InternetProvajder.php";
 require_once "TarifniPaket.php";
 require_once "TarifniDodatak.php";
-//require_once "PrepaidKorisnik.php";
-//require_once "PostpaidKorisnik.php";
+require_once "PrepaidKorisnik.php";
+require_once "PostpaidKorisnik.php";
 require_once "IzradaListinga.php";
 
 // InternetProvajder instance
 $internetProvajder = new InternetProvajder("SBB", array());
 var_dump($internetProvajder);
 echo "<br>";
-
+echo "<br>";
 
 //TarifniPaket instance
 $prepaidSilver = new TarifniPaket(100, 1500, false, 100, 15 );
@@ -21,7 +21,7 @@ $postpaidSilver = new TarifniPaket(100, 2000, true, 200, 10 );
 $postpaidGold = new TarifniPaket(200, 2500, true, 300, 8.33 );
 var_dump($postpaidGold);
 echo "<br>";
-
+echo "<br>";
 
 // TarifniDodatak instance
 $facebook = new TarifniDodatak(400, "Facebook");
@@ -32,7 +32,7 @@ $iptv = new TarifniDodatak(900, "IPTV");
 $fiksna = new TarifniDodatak(600, "Fiksna");
 var_dump($facebook);
 echo "<br>";
-
+echo "<br>";
 
 //ListingUnos se poziva iz surfuj pa dodaj listing unos = new listingunos, takodje se zabelezi zaobracaj poziva kada korisnik surfuje
 
@@ -42,10 +42,11 @@ $prepaidUser1 = new PrepaidKorisnik( 1500, $internetProvajder, "Petar", "Bojovic
 $prepaidUser2 = new PrepaidKorisnik( 500, $internetProvajder, "Frenki", " Radosavljevic", "Frenkijeva 666", 102, $prepaidGold, array());
 var_dump($prepaidUser1);
 echo "<br>";
-
+echo "<br>";
 
 // PrepaidKorisnik instance
 $postpaidUser1 = new PostpaidKorisnik( 500, $internetProvajder, "Todor", "Mihailovic", "Planeta Krypton 77", 103, $postpaidSilver, array($iptv, $fiksna));
 $postpaidUser2 = new PostpaidKorisnik( 0, $internetProvajder, "Mike", "Tyson", "Beverly Hills 23", 104, $postpaidGold, array($instagram, $iptv, $fiksna));
 var_dump($postpaidUser1);
+echo "<br>";
 echo "<br>";
